ca4030cf9f7dcf6dbcfb71f8fbf3227ea66b0fbd
Blai Meléndez Catalán <blaimelcat@gmail.com>
2026-01-04 21:51:15 +0100

    Added first command (incomplete)

--- src/anki_cli/main.py
+++ src/anki_cli/main.py
@@ -1,6 +1,50 @@
-def main():
-    print("Hello from anki-cli!")
+import importlib.metadata
+import sys
+import traceback
+from pathlib import Path
+
+import click
+from anki.collection import Collection
+
+
+def _generic_expection(e: Exception):
+    click.secho(f"Error: {e}\n{traceback.format_exc()}", err=True, fg="red")
+    sys.exit(1)
+
+
+@click.group(invoke_without_command=True)
+@click.option("-v", "--version", is_flag=True, help="Display anki-cli version")
+@click.pass_context
+def anki(ctx: click.Context, version: bool):
+    try:
+        if version:
+            click.secho(f"{importlib.metadata.version('anki-cli')}", fg="cyan")
+            sys.exit(0)
+        elif ctx.invoked_subcommand is None:
+            click.secho(ctx.get_help())
+            sys.exit(0)
+    except Exception as e:
+        _generic_expection(e)
+
+
+@anki.command()
+@click.argument("collection-name", type=str, required=True)
+@click.option(
+    "--collection-dir",
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+    default=Path.cwd(),
+    help="Specify a directory for the new collection",
+)
+def new(collection_name: str, collection_dir: Path):
+    collection_name = (
+        collection_name
+        if collection_name.endswith(".anki2")
+        else collection_name + ".anki2"
+    )
+    collection_path = (collection_dir / collection_name).resolve()
+    col = Collection(str(collection_path))
+    print(col.sched.deck_due_tree())
 
 
 def entry_point():
-    main()
+    anki(obj={})
